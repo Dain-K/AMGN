353b5221a7bdd0f81009db680a1a8ccf281a6a7c
Dain-K <dksudi76@naver.com>
2022-12-03 19:08:34 +0900

    스프링 시큐리티 회원가입 구현

--- amgn/src/main/java/com/cos/amgn/config/SecurityConfig.java
+++ amgn/src/main/java/com/cos/amgn/config/SecurityConfig.java
@@ -1,5 +1,47 @@
 package com.cos.amgn.config;
 
-public class SecurityConfig {
+import com.mysql.cj.protocol.AuthenticationProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+@SuppressWarnings({ "deprecation", "unused" })
+@Configuration // IoC 빈(bean)을 등록
+@EnableWebSecurity(debug = true) // 필터 체인 관리 시작 어노테이션
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // 특정 주소 접근시 권한 및 인증을 위한 어노테이션 활성화
+@ConditionalOnDefaultWebSecurity
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SecurityConfig{
+
+	// 해당 메서드의 리턴디는 오브젝트를 IoC로 등록해줌
+    @Bean 
+    public BCryptPasswordEncoder encodePwd() { // 패스워드 암호화
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    @Order(SecurityProperties.BASIC_AUTH_ORDER)
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.csrf().disable();
+        http.authorizeRequests()
+        	.requestMatchers("/user/**").authenticated()  // 인증만 되면 들어갈 수 있는 주소
+            .anyRequest().permitAll() // 나머지 주소는 모든 권한이 허용
+            .and()
+            .formLogin()
+            .loginPage("/loginForm");
+
+
+        return http.build();
+    }
 }
--- amgn/src/main/java/com/cos/amgn/controller/UserController.java
+++ amgn/src/main/java/com/cos/amgn/controller/UserController.java
@@ -1,15 +1,26 @@
 package com.cos.amgn.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cos.amgn.model.User;
+import com.cos.amgn.repository.UserRepository;
 
 @Controller // View를 리턴하겠다!!
 public class UserController {
 	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
 	@GetMapping({"","/"})
 	public String index() {
-		// 머스테치 기본폴더 src/main/resources/
-		// view 폴더 설정: templates (prefix), mustache (suffix) 생략 가능
 		return "index";
 	}
 	
@@ -22,4 +33,15 @@ public class UserController {
 	public String joinForm() {
 		return "joinForm";
 	}
+	
+	@PostMapping("/join") // 회원가입 입력값 저장
+	public String join(User user) {
+		user.setRole("ROLE_USER");
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+		user.setPassword(encPassword);
+		userRepository.save(user);
+		return "redirect:/loginForm";
+	}
+	
 }
