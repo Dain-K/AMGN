c8cf52185bbf40b6592baa5ea3fb1d6ffca21dc2
Dain-K <dksudi76@naver.com>
2022-12-08 21:47:05 +0900

    프로젝트 최적화 중

--- amgn/src/main/java/com/cos/amgn/config/SecurityConfig.java
+++ amgn/src/main/java/com/cos/amgn/config/SecurityConfig.java
@@ -17,15 +17,17 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @SuppressWarnings({ "deprecation", "unused" })
 @Configuration // IoC 빈(bean)을 등록
-@EnableWebSecurity(debug = true) // 필터 체인 관리 시작 어노테이션
+@EnableWebSecurity(debug = true) // 시큐리티 필터 등록
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true) // 특정 주소 접근시 권한 및 인증을 위한 어노테이션 활성화
 @ConditionalOnDefaultWebSecurity
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class SecurityConfig{
 
 	// 해당 메서드의 리턴디는 오브젝트를 IoC로 등록해줌
+	// BCryptPasswordEncoder는 Spring Security에서 제공하는 비밀번호 암호화 객체
+	// 회원 비밀번호 등로 ㄱ시 해당 메서드를 이용하여 암호화 -> 로그인 처리시 동일한 해시로 비교
     @Bean 
-    public BCryptPasswordEncoder encodePwd() { // 패스워드 암호화
+    public BCryptPasswordEncoder encodePwd() {
         return new BCryptPasswordEncoder();
     }
 
@@ -34,12 +36,12 @@ public class SecurityConfig{
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf().disable();
-        http.authorizeRequests()
+        http.authorizeRequests() // 요청 URL에 따라 접근 권한을 설정
         	.requestMatchers("/user/**").authenticated()  // 인증만 되면 들어갈 수 있는 주소
             .anyRequest().permitAll() // 나머지 주소는 모든 권한이 허용
             .and()
-            .formLogin()
-            .loginPage("/loginForm")
+            .formLogin() // 로그인 폼은
+            .loginPage("/loginForm") // 해당 주소로 로그인 페이지를 호출한다.
             //.usernameParameter("\"email\"")
             .loginProcessingUrl("/login") // /login이 호출되면 시큐리티가 낚아채서 대신 로그인을 해줌
             .defaultSuccessUrl("/");
